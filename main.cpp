7b152124e22ef41c708523ce9492c984485f83e0
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-02-12 04:49:28 +0530

    [add]: sample main.cpp to use memecache using fifo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include "include/fifo_cache_policy.hpp"
+#include "include/cache.hpp"
+
+// alias for easy class typing
+template <typename Key, typename Value>
+using fifo_cache_t = typename caches::fixed_sized_cache<Key, Value, caches::FIFOCachePolicy>;
 
 int main()
 {
 
     std::cout << "Hello Enterpret!\n";
+
+    constexpr std::size_t CACHE_SIZE = 256;
+    fifo_cache_t<std::string, int> cache(CACHE_SIZE);
+
+    cache.Put("Hello", 100);
+    cache.Put("world", 6996);
+
+    std::cout << cache.Get("Hello") << cache.Get("world") << '\n';
 
     return 0;
 }
